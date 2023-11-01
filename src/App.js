71cf196627d9ff17b85628e48b0f44ae40c2f3c1
moto-dev <moto-dev@mbp-dev.local>
2023-11-01 12:30:35 +0900

    complete

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
+import { useRef, useState } from 'react';
 import './App.css';
+import ImageGallery from './ImageGallery';
 
 function App() {
+  const [fetchData, setFetchData] = useState([]);
+  const ref = useRef();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(ref.current.value);
+
+    const endpointURL = `https://pixabay.com/api/?key=40404262-4ceba3469fbe34f976bdea52c&q=${ref.current.value}&image_type=photo`;
+    fetch(endpointURL).then((res) => {
+      return res.json();
+    }).then((data) => {
+      console.log(data.hits);
+      setFetchData(data.hits);
+    });
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="container">
+      <h2>Pixabay Clone</h2>
+      <form onSubmit={(e)=>handleSubmit(e)}>
+        <input type="text" placeholder='画像を検索する' ref={ref} />
+      </form>
+      <ImageGallery fetchData={fetchData} />
+      </div>
   );
 }
 
